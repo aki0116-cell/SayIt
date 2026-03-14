f08177a09c2595c96739a31feae2e43493d3b475
touzz <touzz.0116@gmail.com>
2026-03-15 01:23:01 +0800

    feat: implement User, Word, and Orel classes for object-relationship logic

--- backend/db/crud.py
+++ backend/db/crud.py
@@ -1,22 +1,97 @@
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
 class Word:
     @staticmethod
-    async def insert(db: AsyncSession, user_id: str, word: str, count: int, context: str):
-        
-        sql = text("""
-            INSERT INTO word_records (user_id, word, count, context, timestamp)
-            VALUES (:user_id, :word, :count, :context, GETDATE())
-        """)
+    async def insert_base(db: AsyncSession, word: str, cword: int, context: str):
 
-        await db.execute(sql, {
-            "user_id": user_id,
-            "word": word,
-            "count": count,
-            "context": context
-        })
+        res = await db.execute(
+            text("INSERT INTO objects (type) OUTPUT inserted.object_id VALUES ('word')")
+        )
+        oid = res.scalar()
+
+        await db.execute(
+            text("""
+                INSERT INTO word (object_id, word, cword, context)
+                VALUES (:obj_id, :word, :cword, :context)
+            """),
+            {
+                "obj_id": oid, 
+                "word": word,
+                "cword": cword,
+                "context": context
+            }
+        )
+        await db.commit() 
+        return oid
+
+class User:
+    @staticmethod
+    async def insert_base(db: AsyncSession, uid_str: str, name: str):
+        res = await db.execute(
+            text("INSERT INTO objects (type) OUTPUT inserted.object_id VALUES ('user')")
+        )
+        oid = res.scalar()
+        
+        await db.execute(
+            text("""
+                INSERT INTO user_info (object_id, user_id_string, username)
+                VALUES (:obj_id, :user_id_string, :name)
+            """),
+            {
+                "obj_id": oid,
+                "user_id_string": uid_str,
+                "name": name
+            }
+        )
         await db.commit()
+        return oid
+
+
+class Orel:
+
+    _REL_TYPE_UTW = 'user_to_word'
+    _REL_TYPE_WTU = 'word_to_user'
+
+    @staticmethod
+    async def user_to_word(db: AsyncSession, uid: int, wid: int):
+
+        res = await db.execute(
+            text("""
+                INSERT INTO orel (from_id, to_id, rel_type, count)
+                VALUES (:uid, :wid, :rel, 1)
+            """),
+            {
+                "uid": uid,
+                "wid": wid,
+                "rel": Orel._REL_TYPE_UTW  
+            }
+        )
+        rid = res.scalar()
+        await db.commit()
+        return rid
+    
+    @staticmethod
+    async def word_to_user(db: AsyncSession, wid: int, uid: int):
+        res = await db.execute(
+            text("""
+                INSERT INTO orel (from_id, to_id, rel_type, count)
+                VALUES (:wid, :uid, :rel, 1)
+            """),
+            {
+                "wid": wid,
+                "uid": uid,
+                "rel": Orel._REL_TYPE_WTU  
+            }
+        )
+        rid = res.scalar()
+        await db.commit()
+        return rid
+        
+
 
 class Log:
     @staticmethod
